22007bab530ca2aac1c718423277904520b030af
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2024-12-28 03:24:03 +0100

    refactor: Remove CommandLineRunner initialization for main menu

--- src/main/java/com/eric/onlineresourcemanagementsys/OnlineResourceManagementSysApplication.java
+++ src/main/java/com/eric/onlineresourcemanagementsys/OnlineResourceManagementSysApplication.java
@@ -1,17 +1,7 @@
 package com.eric.onlineresourcemanagementsys;
 
-import com.eric.onlineresourcemanagementsys.menu.MainMenu;
-import com.eric.onlineresourcemanagementsys.repos.UserRepository;
-import com.eric.onlineresourcemanagementsys.services.AuthService;
-import com.eric.onlineresourcemanagementsys.services.UserService;
-import com.eric.onlineresourcemanagementsys.utils.KeyHandler;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.crypto.SecretKey;
-import java.io.File;
 
 @SpringBootApplication
 public class OnlineResourceManagementSysApplication {
@@ -19,17 +9,6 @@ public class OnlineResourceManagementSysApplication {
 
         SpringApplication.run(OnlineResourceManagementSysApplication.class, args);
 
-    }
-
-    @Bean
-    public CommandLineRunner run(MainMenu mainMenu) {
-        return args -> {
-            try {
-                mainMenu.display();
-            } catch (Exception e) {
-                System.err.println("An error occurred while running the application: " + e.getMessage());
-            }
-        };
     }
 
 }
